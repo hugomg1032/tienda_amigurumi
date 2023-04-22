e1c6ca6f481fec781c55c99fe8858962765f466b
Hugo Vega <hugomg1032@gmail.com>
2023-04-22 13:12:00 -0300

    carrusel beta

--- js/hvJs.js
+++ js/hvJs.js
@@ -1,36 +1,39 @@
 
-let iconoBuscar= document.querySelector('#buscar-icono'), 
-    iconoCarrito= document.querySelector('#carrito-icono'), 
-    usuarioIcono= document.querySelector('#usuario-icono'), 
+let iconoBuscar = document.querySelector('#buscar-icono'),
+    iconoCarrito = document.querySelector('#carrito-icono'),
+    usuarioIcono = document.querySelector('#usuario-icono'),
     carrusel = document.querySelector('.carrusel_imagenes'),
     punto = document.querySelectorAll('.punto');
 
 
-function fnAdclassactivo(elmt1,elmt2){
-    elmt1.addEventListener('click',()=>{
-        let elmt= document.querySelector(elmt2);
-        elmt.classList.toggle('activo')});
+function fnAdclassactivo(elmt1, elmt2) {
+    elmt1.addEventListener('click', () => {
+        let elmt = document.querySelector(elmt2);
+        elmt.classList.toggle('activo')
+    });
 }
 
-fnAdclassactivo(usuarioIcono,'.ingreso');
-fnAdclassactivo(iconoBuscar,'.cuadro-busqueda')
-fnAdclassactivo(iconoCarrito,'.carrito-vista')
+fnAdclassactivo(usuarioIcono, '.ingreso');
+fnAdclassactivo(iconoBuscar, '.cuadro-busqueda')
+fnAdclassactivo(iconoCarrito, '.carrito-vista')
 
 
-punto.forEach((puntos, i)=> {
-    punto[i].addEventListener('click',()=> {
-        
-        let ubicacion = i
-        let calculo = ubicacion * -50
+punto.forEach((_a, i) => {
+    punto[i].addEventListener('click', () => {
 
-        carrusel.style.transform = `translateX(${ calculo }%)`;
-        punto.forEach((puntos, i )=> {
-            punto[i].classList.toggle('activo')
+        punto.forEach((_a, i) => {
+            if (punto[i].classList.contains('activo')) {
+                punto[i].classList.remove('activo')
+            }
         })
-        
-        punto[i].classList.add('activo')
 
-    })
-} )
+        let ubicacion = i;
+        let calculo = ubicacion * -25;
+
+        carrusel.style.transform = `translateX(${calculo}%)`;
+        punto[i].classList.add('activo');
+
+    });
+})
 
 
